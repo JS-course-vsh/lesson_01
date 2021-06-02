5f5d7614029ca2402e3ca671bb4ee313e245597b
viktor lut <lvihard@gmail.com>
2021-06-02 20:52:47 +0300

    lesson #4 additional fixed 2

--- js/additionally.js
+++ js/additionally.js
@@ -4,9 +4,9 @@ if((isNaN(+num)) || ((typeof +num) !== 'number') || +num < 0) {
   alert('Введенное значение (' + num + ') некорректно')
 } else {
   const lastnum = num[num.length-1]
-  if(((+lastnum === 0) || (+lastnum >= 5 && +lastnum <= 9)) || (+num >= 5 && +num <= 20)) {
+  if(((lastnum === '0') || (lastnum >= '5' && lastnum <= '9')) || (+num >= 5 && +num <= 20)) {
     alert(num + ' лет');
-  } else if(+lastnum >= 2 && +lastnum <= 4) {
+  } else if(lastnum >= '2' && lastnum <= '4') {
     alert(num + ' года')
   } else alert(num + ' год')
 }
